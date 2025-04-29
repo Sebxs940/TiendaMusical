1eef939d7b32e52cd2998431e9af381de13edcc4
Sebxs940 <sprogramacion940@gmail.com>
2025-04-28 21:25:54 -0500

    Initial Commit

---
+++ src/main/java/com/sebastian/tienda_musical/TiendaMusicalApplication.java
@@ -0,0 +1,13 @@
+package com.sebastian.tienda_musical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMusicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastian/tienda_musical/TiendaMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.tienda_musical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
