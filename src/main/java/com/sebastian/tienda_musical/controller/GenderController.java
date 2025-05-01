b3eee8c90203f8b19a6c8e6c80627440a09f253d
Zedonng <sprogramacion940@gmail.com>
2025-05-01 15:32:08 -0500

    feat: add gender entity, controller, and repository (#2)
    * feat: add gender entity, controller, and repository
    
    * Fix: Rename the variables to plural

---
+++ src/main/java/com/sebastian/tienda_musical/controller/GenderController.java
@@ -0,0 +1,44 @@
+package com.sebastian.tienda_musical.controller;
+
+import com.sebastian.tienda_musical.entity.GenderEntity;
+import com.sebastian.tienda_musical.service.iface.CrudService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genders")
+@RequiredArgsConstructor
+public class GenderController {
+    private final CrudService<GenderEntity, Integer> crudGender;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody GenderEntity gender){
+        crudGender.create(gender);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GenderEntity>> getGenders(){
+        return ResponseEntity.ok(crudGender.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenderEntity> getGenderById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(crudGender.getById(id).get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody GenderEntity gender, @PathVariable("id") Integer id){
+        crudGender.update(gender, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Integer id){
+        crudGender.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
